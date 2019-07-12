cfda20de4fd1df70ea0f32b374a856f075e753f2
blckwngd <blackwinged@online.de>
2019-07-12 15:30:13 +0200

    updated formatting

--- sample.js
+++ sample.js
@@ -6,16 +6,16 @@ var ajna = new AjnaConnector( config );
    ============================= */
    
 ajna.observe( { lat: 1, lon: 2 }, ( objects ) => {
-	// handle available objects
+  // handle available objects
 });
 ajna.on( 'object_entered', ( object ) => {
-	// handle newly appeared object
+  // handle newly appeared object
 } );
 ajna.on( 'object_changed', ( object, old_object ) => {
-	// handle updated object
+  // handle updated object
 } );
 ajna.on( 'object_left', ( old_object ) => {
-	// handle removed object
+  // handle removed object
 } );
 
 
@@ -27,11 +27,11 @@ ajna.on( 'object_left', ( old_object ) => {
 var me = ajna.getObject( 'myObjId' );
 me.move( 'forward', 5.0 );
 me.on( 'action_triggered', ( sender, name, parameters ) => {
-	// $sender triggered action $name on our object
-	// post a message back to the sender
-	if ( name == 'tickle' ) {
-		me.sendTo( sender, "say", { text: 'stop tickling me :-P' } );
-	} else {
-		me.sendTo( sender, "say", { text: 'I don´t understand.' } );
-	}
+  // $sender triggered action $name on our object
+  // post a message back to the sender
+  if ( name == 'tickle' ) {
+    me.sendTo( sender, "say", { text: 'stop tickling me :-P' } );
+  } else {
+    me.sendTo( sender, "say", { text: 'I don´t understand.' } );
+  }
 } );
