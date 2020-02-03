f6468e8f950aefe316d8eebd49b976b17f542d99
Andreas Barth <blackwinged@online.de>
2020-02-03 21:32:59 +0100

    added firebase config

---
+++ samples/config.js
@@ -0,0 +1,9 @@
+exports.firebaseConfig = {
+  apiKey: "AIzaSyAnRCIFgGZwGGOmjZ7R5juJ4LD34rv86iE",
+  authDomain: "fireajna.firebaseapp.com",
+  databaseURL: "https://fireajna.firebaseio.com",
+  projectId: "fireajna",
+  storageBucket: "fireajna.appspot.com",
+  messagingSenderId: "1041333232578",
+  appId: "1:1041333232578:web:d6e4f6941228c2ea"
+};
