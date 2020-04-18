7f66a9180e093623e4523a68a0c06d80ea2071aa
Andreas Barth <blackwinged@online.de>
2020-04-18 23:00:42 +0200

    Cloudfunctions

---
+++ CloudFunctions/functions/index.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+// DEPLOY:
+// firebase deploy --only functions
+'use strict';
+
+// [START all]
+// [START import]
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+
+// [END import]
+
+ 
+exports.objectDeletionHandler = functions.firestore
+    .document('objects/{objId}')
+    .onDelete((snap, context) => {
+
+      console.log("SNAP");
+      console.log(snap);
+      let path = `objects/${snap.id}/inbox`;
+      console.log(`path=${path}`);
+      
+      return firebase_tools.firestore.delete(path, {
+        project: process.env.GCLOUD_PROJECT,
+        recursive: true,
+        yes: true,
+        token: functions.config().fb.token
+      })
+      .then(() => {
+        console.log("DELETION DONE!");
+      });
+});
+
+
+    
+// [END all]
